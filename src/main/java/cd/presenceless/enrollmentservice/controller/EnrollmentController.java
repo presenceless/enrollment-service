6265d557b9f5660e710f20f5c4dfa25082e590ff
lucienmakutano <lucien.programmer@gmail.com>
2023-12-22 16:43:28 +0300

    Service communication through messaging system(rabbitmq)

--- src/main/java/cd/presenceless/enrollmentservice/controller/EnrollmentController.java
+++ src/main/java/cd/presenceless/enrollmentservice/controller/EnrollmentController.java
@@ -3,7 +3,7 @@ package cd.presenceless.enrollmentservice.controller;
 import cd.presenceless.enrollmentservice.data.EnrolmentData;
 import cd.presenceless.enrollmentservice.data.Response;
 import jakarta.validation.Valid;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,25 +16,19 @@ import java.util.Date;
 @RestController
 @RequestMapping("/api/v1/enroll")
 public class EnrollmentController {
-    private final RabbitTemplate template;
+    private final AmqpTemplate template;
 
     @Value("${rabbitmq.enrollment.exchange}")
     private String exchange;
     @Value("${rabbitmq.enrollment.routing-key.validation}")
     private String routingKey;
 
-    public EnrollmentController(RabbitTemplate template) {
+    public EnrollmentController(AmqpTemplate template) {
         this.template = template;
     }
 
     @PostMapping
     public ResponseEntity<Response> enroll(@Valid @RequestBody EnrolmentData enrollmentData) {
-        assert template != null;
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         template.convertAndSend(exchange, routingKey, enrollmentData);
         return ResponseEntity.ok(new Response("Enrollment request sent", "in-progress", new Date()));
     }
